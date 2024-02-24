bb450f5e34cc45a9e795899a5086ce8f34b6a391
Eyan <161081017+Eyan17@users.noreply.github.com>
2024-02-24 15:52:04 +0100

    Add files via upload

--- Main.cs
+++ Main.cs
@@ -6,6 +6,12 @@ using BepInEx.Logging;
 using BepInEx.Configuration;
 using Unity.Netcode;
 using System.Linq;
+using Amrv.ConfigurableCompany;
+using Amrv.ConfigurableCompany.content.model;
+using Amrv.ConfigurableCompany.content.model.events;
+using Amrv.ConfigurableCompany.content.model.data;
+using System;
+using UnityEngine.Experimental.Rendering;
 
 namespace LethalCompanyTemplate
 {
@@ -18,10 +24,16 @@ namespace LethalCompanyTemplate
         private const string modVersion = "1.0.1";
 
         public static List<string> whitelist;
+        public static List<string> disconnectedPlayers;
 
         public ConfigEntry<bool> isWhitelistOn;
         private ConfigEntry<string> allowedUsernames;
 
+        private static ConfigurationCategory whitelistCategory = LethalConfiguration.CreateCategory().SetID("eyan17_whitelist").SetName("Whitelist").Build();
+
+        Configuration whitelistInput = LethalConfiguration.CreateConfig().SetID("eyan17_whitelist_input").SetName("Allowed Players").SetTooltip("A list of usernames seperated by a semicolon. All spaces before and after the name are trimed.").SetCategory(whitelistCategory).SetType(ConfigurationTypes.String).SetValue("Player").Build();
+        Configuration whitelistToggle = LethalConfiguration.CreateConfig().SetID("eyan17_whitelist_toggle").SetName("Whitelist Toggle").SetTooltip("Toggles whether the whitelist is on.").SetCategory(whitelistCategory).SetType(ConfigurationTypes.Boolean).SetValue(true).Build();
+        
         internal static ManualLogSource mls;
 
         private void Awake()
@@ -32,11 +44,17 @@ namespace LethalCompanyTemplate
             isWhitelistOn = Config.Bind("General", "isWhitelistOn", true, "Toggles whether the whitelist is on.");
             allowedUsernames = Config.Bind("Whitelist", "allowedUsenames", "Player", "A list of usernames seperated by a semicolon. All spaces before and after the name are trimed.");
 
-            var a = splitString(allowedUsernames.Value);
-
-            whitelist = a;
-
             mls.LogInfo("Whitelisted players:");
+
+            whitelistToggle.TrySet(isWhitelistOn.Value, ChangeReason.READ_FROM_FILE);
+            whitelistInput.TrySet(allowedUsernames.Value, ChangeReason.READ_FROM_FILE);
+
+            Events.ConfigurationChanged.Listeners += OnConfigUIChanged;
+            isWhitelistOn.SettingChanged += OnConfigFileChanged;
+            allowedUsernames.SettingChanged += OnConfigFileChanged;
+
+            var a = splitString(allowedUsernames.Value);
+            whitelist = a;
 
             for (int i = 0; i < whitelist.Count; i++)
             {
@@ -49,6 +67,25 @@ namespace LethalCompanyTemplate
                 mls.LogInfo("Update has been patched.");
             }
         }
+
+        private void OnConfigUIChanged(object sender, EventArgs e)
+        {
+            isWhitelistOn.Value = whitelistToggle.Get<bool>();
+            allowedUsernames.Value = whitelistInput.Get<string>();
+            var a = splitString(allowedUsernames.Value);
+            whitelist = a;
+            mls.LogInfo("Saved settings to config from UI.");
+        }
+
+        private void OnConfigFileChanged(object sender, EventArgs args)
+        {
+            whitelistToggle.TrySet(isWhitelistOn.Value, ChangeReason.READ_FROM_FILE);
+            whitelistInput.TrySet(allowedUsernames.Value, ChangeReason.READ_FROM_FILE);
+            var a = splitString(allowedUsernames.Value);
+            whitelist = a;
+            mls.LogInfo("Read settings from config.");
+        }
+
         static List<string> splitString(string input)
         {
             List<string> result = new List<string>();
@@ -90,13 +127,14 @@ namespace LethalCompanyTemplate
 
             if (!Whitelist.whitelist.Contains<string>(___playerUsername))
             {
-                Whitelist.mls.LogInfo("Checking whitelist...");
                 NetworkManager.Singleton.DisconnectClient(___actualClientId, "Not on whitelist.");
+
+
+                if (Whitelist.disconnectedPlayers.Contains(___playerUsername)) return;
+
+                Whitelist.disconnectedPlayers.Add(___playerUsername);
                 Whitelist.mls.LogInfo("Disconnected " + ___playerUsername + "; " + "Client id: " + ___actualClientId);
             }
         }
     }
 }
-
-
-
